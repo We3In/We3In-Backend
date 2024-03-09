6f5dd4339bc9275a92c85e219948b24ba2421776
Buğra Okumuş <saltuk.bugraokumus@gmail.com>
2024-03-10 00:32:20 +0300

    refator : add publickey AppUser

--- Core/Domain/Entities/Identity/AppUser.cs
+++ Core/Domain/Entities/Identity/AppUser.cs
@@ -10,5 +10,10 @@ namespace Domain.Entities.Identity
     public class AppUser : IdentityUser<string>
     {
         public string NameSurname { get; set; }
+        public string PublicKey { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenEndDate { get; set; }
+
     }
 }
