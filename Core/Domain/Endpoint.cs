dde91d435068229475e3c65bc70e435a52b6ecd9
Buğra Okumuş <saltuk.bugraokumus@gmail.com>
2024-03-10 01:00:15 +0300

    add page entity

---
+++ Core/Domain/Endpoint.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Domain.Entities.Common;
+using Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Endpoint : BaseEntity
+    {
+        public Endpoint()
+        {
+            Roles = new HashSet<AppRole>();
+        }
+        public string ActionType { get; set; }
+        public string HttpType { get; set; }
+        public string Definition { get; set; }
+        public string Code { get; set; }
+
+        public Page Page { get; set; }
+        public ICollection<AppRole> Roles { get; set; }
+    }
+}
+}
---
+++ Core/Domain/Entities/Page.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Page : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Endpoint> Endpoints { get; set; }
+    }
+}
